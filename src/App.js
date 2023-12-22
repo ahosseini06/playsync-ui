4fd1dc337c38bc7640df422450a101591ea076d9
ahosseini06 <ali20060101@gmail.com>
2023-12-21 18:27:19 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
+import { useGetEntitiesQuery } from "./services/lastmeal";
 
 function App() {
+  const {data: t} = useGetEntitiesQuery
   return (
     <ChakraProvider>
     <div className="App">
-      
+      {t.data.map(tr => <h4>{tr.attributes.name}</h4>)}
     </div>
     </ChakraProvider>
   );
