5c2e2c70b056d97597eeb03df05940d453ead2eb
ahosseini06 <ali20060101@gmail.com>
2023-12-24 13:57:45 -0800

    redux install

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
-import { useGetEntitiesQuery } from "./services/lastmeal";
+import { useGetEntitiesQuery } from "./services/playmaker";
+import {Provider} from "react-redux"
 
 function App() {
-  const {data: t} = useGetEntitiesQuery
+  const {data: t} = useGetEntitiesQuery({name: "tournament", populate:true})
   return (
     <ChakraProvider>
     <div className="App">
-      {t.data.map(tr => <h4>{tr.attributes.name}</h4>)}
+      {t && t.data.map(tr => <h4>{tr.attributes.name}</h4>)}
     </div>
     </ChakraProvider>
   );
--- src/services/playmaker.js
+++ src/services/playmaker.js
@@ -1,9 +1,6 @@
 // Need to use the React-specific entry point to import createApi
 import pluralize from 'pluralize';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { io } from 'socket.io-client';
-import { toast } from 'react-toastify';
-import { showAlert } from '../redux/actions';
 
 // Define a service using a base URL and expected endpoints
 export const playmakerApi = createApi({
@@ -173,131 +170,131 @@ export const playmakerApi = createApi({
         }
       },
     }),
-    getMessages: builder.query({
-      query: (gameroom) => `messages?filters[gameroom][id]=${gameroom}&populate=*&pagination[pageSize]=1000`,
-      async onCacheEntryAdded(
-        arg,
-        { updateCachedData, cacheDataLoaded, cacheEntryRemoved, dispatch }
-      ) {
-        // create a websocket connection when the cache subscription starts        
-        const socket = io(process.env.REACT_APP_API_HOST);
-        try {
-          socket.on('connect', () => {
-            console.log("connected to socket")
-            //join the game room
-            socket.emit("join", { gameroom: arg }, (error) => {
-              console.log("joined the room:" + arg)
-              if (error) return alert(error);
-            });
-          });
+//     getMessages: builder.query({
+//       query: (gameroom) => `messages?filters[gameroom][id]=${gameroom}&populate=*&pagination[pageSize]=1000`,
+//       async onCacheEntryAdded(
+//         arg,
+//         { updateCachedData, cacheDataLoaded, cacheEntryRemoved, dispatch }
+//       ) {
+//         // create a websocket connection when the cache subscription starts        
+//         const socket = io(process.env.REACT_APP_API_HOST);
+//         try {
+//           socket.on('connect', () => {
+//             console.log("connected to socket")
+//             //join the game room
+//             socket.emit("join", { gameroom: arg }, (error) => {
+//               console.log("joined the room:" + arg)
+//               if (error) return alert(error);
+//             });
+//           });
 
-          // wait for the initial query to resolve before proceeding
-          await cacheDataLoaded
-          // when data is received from the socket connection to the server,
-          // if it is a message and for the appropriate channel,
-          // update our query result with the received message
-          socket.on('message', (message) => {
-            updateCachedData((draft) => {
-              draft.data.push(message);
-              dispatch({
-                type: `govsimApi/invalidateTags`,
-                payload: [{ type: 'promise', id: 'LIST' }],
-              });
-            });
-          });
+//           // wait for the initial query to resolve before proceeding
+//           await cacheDataLoaded
+//           // when data is received from the socket connection to the server,
+//           // if it is a message and for the appropriate channel,
+//           // update our query result with the received message
+//           socket.on('message', (message) => {
+//             updateCachedData((draft) => {
+//               draft.data.push(message);
+//               dispatch({
+//                 type: `govsimApi/invalidateTags`,
+//                 payload: [{ type: 'promise', id: 'LIST' }],
+//               });
+//             });
+//           });
 
-          socket.on('new_party', (message) => {
-            toast('A new party has joined the Game : ' + message.name)
-            dispatch({
-              type: `govsimApi/invalidateTags`,
-              payload: [{ type: 'party', id: 'LIST' }],
-            });
-          });
-
-
-          socket.on('challenge_updated', (message) => {
-            const user = JSON.parse(localStorage.getItem('user')).user
-            if (message.to_player.id === user.id || message.from_player.id === user.id) {
-              dispatch(showAlert({
-                show: false
-              }));
-              //if the status is Accepted, then redirect to the game
-              if (message.status === 'ACCEPTED') {
-                window.location = '/game/' + message.game.url + '/' + message.id;
-              }
-
-            }
-          });
-
-          socket.on('challenge', (message) => {
-            const user = JSON.parse(localStorage.getItem('user')).user
-            if (message.to_player.id === user.id) {
-              dispatch(showAlert({
-                show: true,
-                title: 'Challenge',
-                showSpinner: true,
-                message: 'You have been challenged by ' + message.from_player.name + ' for a game of ' + message.game.name,
-                msgBody: message,
-                showConfirmButton: true,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                showDenyButton: true,
-                showCancelButton: false,
-                confirmButtonText: 'Accept',
-                denyButtonText: `Reject`,
-              }));
-            }
-            if (message.from_player.id === user.id) {
-              dispatch(showAlert({
-                show: true,
-                title: 'Challenge',
-                showSpinner: true,
-                message: 'Your challenge has been sent to ' + message.to_player.name + ' for a game of ' + message.game.name,
-                msgBody: message,
-                timer: 60000,
-                timerProgressBar: true,
-                showConfirmButton: false,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                showCancelButton: true
-              }));
-            }
-          });
-
-          socket.on('election_finished', (message) => {
-            dispatch(showAlert({
-              show: false,
-            }));
-
-            setTimeout(() => {
-              dispatch(showAlert({
-                show: true,
-                showSpinner: false,
-                title: 'Elections',
-                message: 'Elections result are out',
-              }));
-            }, 100);
+//           socket.on('new_party', (message) => {
+//             toast('A new party has joined the Game : ' + message.name)
+//             dispatch({
+//               type: `govsimApi/invalidateTags`,
+//               payload: [{ type: 'party', id: 'LIST' }],
+//             });
+//           });
 
 
-            dispatch({
-              type: `govsimApi/invalidateTags`,
-              payload: [{ type: 'country', id: 'LIST' }, { type: 'party', id: 'LIST' }],
-            });
-          });
+//           socket.on('challenge_updated', (message) => {
+//             const user = JSON.parse(localStorage.getItem('user')).user
+//             if (message.to_player.id === user.id || message.from_player.id === user.id) {
+//               dispatch(showAlert({
+//                 show: false
+//               }));
+//               //if the status is Accepted, then redirect to the game
+//               if (message.status === 'ACCEPTED') {
+//                 window.location = '/game/' + message.game.url + '/' + message.id;
+//               }
+
+//             }
+//           });
+
+//           socket.on('challenge', (message) => {
+//             const user = JSON.parse(localStorage.getItem('user')).user
+//             if (message.to_player.id === user.id) {
+//               dispatch(showAlert({
+//                 show: true,
+//                 title: 'Challenge',
+//                 showSpinner: true,
+//                 message: 'You have been challenged by ' + message.from_player.name + ' for a game of ' + message.game.name,
+//                 msgBody: message,
+//                 showConfirmButton: true,
+//                 allowOutsideClick: false,
+//                 allowEscapeKey: false,
+//                 showDenyButton: true,
+//                 showCancelButton: false,
+//                 confirmButtonText: 'Accept',
+//                 denyButtonText: `Reject`,
+//               }));
+//             }
+//             if (message.from_player.id === user.id) {
+//               dispatch(showAlert({
+//                 show: true,
+//                 title: 'Challenge',
+//                 showSpinner: true,
+//                 message: 'Your challenge has been sent to ' + message.to_player.name + ' for a game of ' + message.game.name,
+//                 msgBody: message,
+//                 timer: 60000,
+//                 timerProgressBar: true,
+//                 showConfirmButton: false,
+//                 allowOutsideClick: false,
+//                 allowEscapeKey: false,
+//                 showCancelButton: true
+//               }));
+//             }
+//           });
+
+//           socket.on('election_finished', (message) => {
+//             dispatch(showAlert({
+//               show: false,
+//             }));
+
+//             setTimeout(() => {
+//               dispatch(showAlert({
+//                 show: true,
+//                 showSpinner: false,
+//                 title: 'Elections',
+//                 message: 'Elections result are out',
+//               }));
+//             }, 100);
 
 
-        } catch {
-          // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
-          // in which case `cacheDataLoaded` will throw
-        }
-        // cacheEntryRemoved will resolve when the cache subscription is no longer active
-        await cacheEntryRemoved
-        // perform cleanup steps once the `cacheEntryRemoved` promise resolves
-        socket.off('connect');
-        socket.off('message');
-      },
-      providesTags: (result, error, arg) => [{ type: 'message', id: 'LIST' }],
-    })
+//             dispatch({
+//               type: `govsimApi/invalidateTags`,
+//               payload: [{ type: 'country', id: 'LIST' }, { type: 'party', id: 'LIST' }],
+//             });
+//           });
+
+
+//         } catch {
+//           // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
+//           // in which case `cacheDataLoaded` will throw
+//         }
+//         // cacheEntryRemoved will resolve when the cache subscription is no longer active
+//         await cacheEntryRemoved
+//         // perform cleanup steps once the `cacheEntryRemoved` promise resolves
+//         socket.off('connect');
+//         socket.off('message');
+//       },
+//       providesTags: (result, error, arg) => [{ type: 'message', id: 'LIST' }],
+//     })
   })
 })
 
@@ -317,5 +314,4 @@ export const {
   useUpdateMessagesReadMutation,
   useCreateSessionMutation,
   useGetSessionMutation,
-  useGetMessagesQuery
 } = playmakerApi
