782064b768d76ac37c871bb0e89084cc000a8421
ahosseini06 <ali20060101@gmail.com>
2024-01-27 23:07:48 -0800

    login 3
    Final version, tournament bracket, mouse interaction, and input field interaction.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,8 @@ import { FaEye } from "react-icons/fa";
 import styles from "./styles/Login.module.css";
 import './styles/interactive.css'
 import './styles/Login.css'
+import Bracket from "./Bracket";
+import bracket from './bracket.png'
 
 const Login = () => {
   function getCssPosition(x, y, offsetXPercent, offsetYPercent) {
@@ -22,6 +24,8 @@ const Login = () => {
   
   const [position, setPosition] = useState({ right: 0, top: 0 });
   const [target, setTarget] = useState({ x: 0, y: 0 });
+  // number between 1 and 50
+  const [progress, setProgress] = useState(0);
 
   useEffect(() => {
     const move = () => {
@@ -39,13 +43,22 @@ const Login = () => {
       window.removeEventListener('mousemove', move);
     };
   }, [target]);
+  
 
   const [loginFailed, setLoginFailed] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [identifier, setIdentifier] = useState("");
   const [pass, setPass] = useState("");
 
+  
+  const updateProgress = () => {
+    setProgress(identifier.length + pass.length) ;
+  }
 
+  useEffect(() => {
+    updateProgress();
+  }, [identifier, pass]);
+  
   const onSubmit = async () => {
     console.log("identifier", identifier)
     console.log("pass", pass)
@@ -68,8 +81,7 @@ const Login = () => {
     <>
     
     <div className={styles.login}>
-    <div className={styles[`background`]}>
-     <div
+    <div
       className="interactive"
       style={{
         position: 'absolute',
@@ -77,30 +89,36 @@ const Login = () => {
         top: position.top,
       }}
     />
-    </div>
+    <div className="fill" style={{transition: "all 0.6s ease-in-out", position: "absolute", display: "flex", height: "100%", color: "black", filter: "blur(10px)", width: `${progress}%`}}></div>
+    <div className="fill" style={{transition: "all 0.6s ease-in-out", position: "absolute", display: "flex", height: "100%", color: "black", filter: "blur(10px)", width: `${progress}%`, alignSelf: "end"}}></div>
+    <img className={styles.img} src={bracket} alt="failed to load"></img>
     <div className={styles.container}>
       <div className={styles.header}>Administrator Login</div>
       <div className={styles[`login-form`]}>
       {loginFailed && <div className={styles[`failed-login`]}>Wrong username or password!</div>}
       <div className={styles[`input-wrapper`]}>
       <input
+        autoComplete="new-password"
         type="text"
         placeholder="username or email"
         value={identifier}
         onChange={(e) => {
           setIdentifier(e.target.value);
           console.log(e.target.value)
+          updateProgress();
         }}
       />
       </div>
       <div className={styles[`input-wrapper`]}>
         <input
+        autoComplete="new-password"
           type={showPassword ? "text" : "password"}
           placeholder="password"
           value={pass}
           onChange={(e) => {
             setPass(e.target.value);
             console.log(e.target.value)
+            updateProgress();
           }}
         />
         {showPassword ? (
@@ -109,7 +127,7 @@ const Login = () => {
           <PiEyeClosedBold onClick={toggleShowPassword} id="icon"/>
         )}
       </div>
-      <button className={styles[`login-btn`]}onClick={onSubmit}>Login</button>
+      <button className={styles[`login-btn`]}onClick={onSubmit} onMouseEnter={() => setProgress(50)} onMouseLeave={()=> updateProgress()}>Login</button>
       </div>
       </div>
     </div>
